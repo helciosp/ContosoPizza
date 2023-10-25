6a3f04fcdfaa2810d04cc6c40b41887d4e0f32c9
helciosp <helciosp43@gmail.com>
2023-10-25 16:37:36 -0300

    Método getAll e get(int id)

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -1,3 +1,5 @@
+using ContosoPizza.Models;
+using ContosoPizza.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ContosoPizza.Controllers;
@@ -8,5 +10,19 @@ public class PizzaController : ControllerBase
 {
     public PizzaController()
     {
+    }
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll() =>
+            PizzaService.GetAll();
+
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza == null)
+            return NotFound();
+
+        return pizza;
     }
 }
